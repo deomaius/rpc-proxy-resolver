b510c724e6165c12f8044ecedd2a1247f54aeb25
gozzy <samuel.jj.gosling@gmail.com>
2022-09-02 06:36:32 +0000

    remove rpc-registry.ts

--- rpc-registry.ts
+++
@@ -1,8 +0,0 @@
-export default [
-  "https://rpc.ap-southeast-1.gateway.fm/v1/gnosis/non-archival/mainnet",
-  "https://gnosis-mainnet.public.blastapi.io",
-  "https://gno.getblock.io/mainnet/?api_key=f828bb49-b5d1-4f45-a378-345dfeb03ad7",
-  "https://gno.getblock.io/mainnet/?api_key=40b46fcc-35e1-4372-9c7c-a5b930ad42f4",
-  "https://frequent-thrumming-yard.xdai.discover.quiknode.pro/8c43dc331491d83fd62c07a4e5d960b2d08baa86",
-  "https://virulent-shy-card.xdai.discover.quiknode.pro/575439cb4077a4f61504744ff9f7f9838cba3ce4"
-]
